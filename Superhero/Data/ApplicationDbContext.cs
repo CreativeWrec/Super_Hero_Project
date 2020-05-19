0cca9979b45a41ae88827598a2bb7cdce90949d2
Kevin Harvey Jr <LordTy@gmail.com>
2020-05-18 21:30:36 -0500

    Intial_Commit

---
+++ Superhero/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Superhero.Models;
+
+namespace Superhero.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            
+        }
+
+        public DbSet<Hero> SuperHeroes { get; set; }
+    }
+}
